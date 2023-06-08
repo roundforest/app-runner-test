4639d4fe1c0332f8120e94bb958888759cb8680e
Gal <galhen400@gmail.com>
2023-06-08 06:13:50 +0000

    first temp commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// ESM
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Run the server!
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
+  
+  /**
+   * Run the server!
+   */
+  const start = async () => {
+    try {
+      await fastify.listen({ port: 3000 })
+      console.log('server is running!')
+    } catch (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  }
+  start()  
