5bda02b92d03d47722a35c5493386b5f5ada52cb
Gal Hen <galhen@roundforest.com>
2023-06-08 10:06:45 +0300

    second commit

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const fastify = Fastify({
 // Run the server!
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
-  })
+})
+
+// Run the server!
+fastify.get('/health', async (request, reply) => {
+  return {statusCode: 200, message: 'Healthy!'}
+})
   
-  /**
-   * Run the server!
-   */
-  const start = async () => {
-    try {
-      await fastify.listen({ port: 8080 })
-      console.log('server is running!')
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    const runPort = +process.env.PORT || 3030
+    const app = await fastify.listen({ port: runPort })
+    console.log(`server is running!, app: ${app} on port: ${runPort}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
   }
-  start()  
+}
+start()  
