6c23ae4f8a25083ef501030add8a98f3a85f76d2
Gal <galhen@roundforest.com>
2023-06-08 09:32:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ fastify.get('/', async (request, reply) => {
    */
   const start = async () => {
     try {
-      await fastify.listen({ port: 3000 })
+      await fastify.listen({ port: 8080 })
       console.log('server is running!')
     } catch (err) {
       fastify.log.error(err)
