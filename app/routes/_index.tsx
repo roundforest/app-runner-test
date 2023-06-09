7e72dff02dc59845b74e661168182b37abda834f
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2023-06-09 10:11:52 +0300

    new build

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,33 +9,19 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        fontSize: "40px",
+        lineHeight: "1.8",
+        backgroundColor: "grey",
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "50px",
+        height: "100vh",
+      }}
+    >
+      <h1>HELLO GALOS</h1>
     </div>
   );
 }
