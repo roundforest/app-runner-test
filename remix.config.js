3bf473185b423907d4a6f416808069aea9789145
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2023-07-17 13:32:15 +0300

    add docker config

--- remix.config.js
+++ remix.config.js
@@ -9,6 +9,7 @@ module.exports = {
   // publicPath: "/build/",
   serverModuleFormat: 'cjs',
   future: {
+    v2_headers: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
